c731eb0ed038b38cccef3f4c831eaf9b66a90ef7
kleuvyn <kleuvyn@hotmail.com>
2023-05-11 22:30:05 -0300

    Update Exercício 8.py

--- Exercício 8.py
+++ Exercício 8.py
@@ -7,4 +7,4 @@ caracteristicas = {'vergonha':{'calma' 'beleza' :{'extrovertido' }}}
 sete_pecados = {'gula' 'avareza' :{'luxúria' 'ira' 'inveja' :{'preguiça' 'soberba' }}}
 numeros = {'21' '24' '36' '85' '97' :{'15' '9' '87' '95' '67' :{'55' '44' '98' '43'}}}
 
-print()
+print(numeros)
