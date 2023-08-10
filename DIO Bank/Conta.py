3356c36ae0f24aefff25f28f976f39d73c7a7c56
kleuvyn <kleuvyn@hotmail.com>
2023-08-10 10:16:33 -0300

    Olá, Bem-vindos ao código do desafio Bank em Python!
    Estou empolgada em compartilhar este projeto com vocês. Aqui, você encontrará o resultado de muito esforço, aprendizado e dedicação. Esse banco foi desenvolvido como parte de um desafio para aprimorar minhas habilidades de programação em Python e conhecer melhor os conceitos.

--- DIO Bank/Conta.py
+++ DIO Bank/Conta.py
@@ -1,14 +1,17 @@
+import random
+
 class ContaBancaria:
 
     def __init__(self):
         self.contas = {}
+        self.agencia = "326644"
 
     def menu(self):
         return """
 [d] = Depositar
 [s] = Sacar
 [e] = Extrato
-[b] = Saldo
+[b] = Saldo+
 
 [nc] = Nova Conta
 [nu] = Novo Usuario
@@ -17,27 +20,26 @@ class ContaBancaria:
 
 => """
 
-    def criar_cliente(self):
+    def novo_usuario(self):
         cpf = input("CPF do titular da nova conta (somente números): ")
         if cpf in self.contas:
             print("Cliente já existe com esse CPF.")
         else:
-            self.contas[cpf] = {"saldo": 0, "extrato": [], "numero_do_saque": 0}
+            nome = input("Nome Completo: ")
+            endereco = input("Endereço: ")
+            email = input("E-mail: ")
+            numero_conta = self.gerar_numero_conta()
+            self.contas[cpf] = {"nome": nome, "saldo": 0, "extrato": [], "numero_conta": numero_conta, "endereco": endereco, "email": email}
             print("Cliente criado com sucesso.")
 
-    def novo_usuario(self):
-        cpf = input("CPF do titular da conta (somente números): ")
-        if cpf in self.contas:
-            nome = input("Nome Completo: ")
-            self.contas[cpf]["nome"] = nome
-            print("Usuário criado com sucesso.")
-        else:
-            print("Cliente não encontrado com esse CPF.")
+    def gerar_numero_conta(self):
+        return str(random.randint(10000, 99999))
+
 
     def lista_contas(self):
         print("Lista de Contas:")
         for cpf, dados in self.contas.items():
-            print(f"CPF: {cpf}, Nome: {dados.get('nome', 'Não informado')}")
+            print(f"CPF: {cpf}, Nome: {dados.get('nome', 'Não informado')}, Número da Conta: {dados.get('numero_conta', 'Não informado')}, Endereço: {dados.get('endereco', 'Não informado')}, E-mail: {dados.get('email', 'Não informado')}")
 
 
 def main():
@@ -69,3 +71,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
